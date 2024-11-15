d028278d8e283c02cc5cc254b496ba127dcc637a
Gabrielavaninagomez <gabrielavaninagomez.com>
2024-11-14 21:36:58 -0300

    primer commit?

---
+++ README.MD.txt
@@ -0,0 +1,4 @@
+Oveja Negra es una cafeteria en el barrio de caballito, 
+en ella se puede ver la carta, realizar pedidos y estar actualizado en las ofertas, eventos y demas
+Realizado por Gabriela Vanina Gomez
+gabrielavaninagomez@gmail.com
---
+++ js/javascript.js
@@ -0,0 +1,12 @@
+/*var usuario = prompt ("ingrese su nombre:");
+document.write ("hola")
+console.log ("hola mundo");
+
+var usuario = "Matias";
+
+console.info(usuario);*/
+
+var numeroA= parseInt(prompt("ingrese el primer numero: "));
+var numeroB= parseInt(prompt("ingrese el segundo numero: "));
+var operacion = numeroA+numeroB
+console.log (operacion)
